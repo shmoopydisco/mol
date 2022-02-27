68a40bd80d603f18abf6da53c0654467a90416a1
shmoopydisco <deadphaker@gmail.com>
2022-02-27 16:21:34 +0200

    add the st_jsme module into the git project

---
+++ st_jsme/frontend/build/precache-manifest.7adc27224059729eef88404bb69b7b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6ce55a72442c5c3d9c66b00f853a2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "961ffb75ebcb311e6d3f",
+    "url": "./static/js/2.3f902baa.chunk.js"
+  },
+  {
+    "revision": "c4806c150d49abac1ee67e2a3f6f71b4",
+    "url": "./static/js/2.3f902baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70291650cb0d8d3decca",
+    "url": "./static/js/main.26b8b274.chunk.js"
+  },
+  {
+    "revision": "7c417fdcdd265b5366e4",
+    "url": "./static/js/runtime-main.119b752e.js"
+  }
+]);
